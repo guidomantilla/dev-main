a0b99c00a49632948f0710f52aa2c9884fd2c544
Guido Mauricio Mantilla Tarazona <guido.mantilla@yahoo.com>
2020-06-01 13:53:46 -0500

    FIx Commit

---
+++ build.sh
@@ -0,0 +1,27 @@
+echo "" && echo "##### LIMPIANDO AMBIENTE DOCKER LOCAL #####" && echo ""
+docker container rm --force dev-mysql
+docker container rm --force dev-ldap-apache-server
+docker container rm --force dev-sonarqube-server
+docker network rm dev-network
+
+echo "" && echo "##### DESCARGANDO PROYECTOS GITHUB #####" && echo ""
+cd ..
+WORK_DIR=$PWD
+
+rm -rf dev-mysql
+rm -rf dev-ldap-apache-server
+rm -rf dev-sonarqube-server
+
+git clone git@github.com:guidomantilla/dev-mysql.git
+echo ""
+git clone https://github.com/guidomantilla/dev-java-lib
+echo ""
+git clone https://github.com/guidomantilla/dev-ldap-apache-server
+
+echo "" && echo "##### PROYECTOS DESCARGADOS: #####"  && echo ""
+echo "$(ls -h)"
+
+echo "" && echo "##### CREANDO AMBIENTE DOCKER LOCAL #####"  && echo ""
+cd $WORK_DIR/dev-main
+docker-compose up -d --build
+
